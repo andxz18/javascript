c1b3e90b5573b7d61d69d763068a3cb9938a4794
André Macuco <amacuco633@gmail.com>
2022-05-27 19:56:45 -0300

    Atualizado 27/05/22

--- 002_fundamentos/aula_005/numeros.js
+++ 002_fundamentos/aula_005/numeros.js
@@ -1,4 +1,17 @@
 const peso1 = 1.0;
 const peso2 = Number("2.0");
 
-console.log("");
+console.log("peso1, peso2");
+console.log(Number.isInteger(peso1));
+console.log(Number.isInteger(peso2));
+
+const avaliacao1 = 9.871;
+const avaliacao2 = 6.871;
+
+const totatl = avaliacao1 * peso1 + avaliacao2 * peso2;
+const media = totatl / (peso1 + peso2);
+
+console.log(media.toFixed(2));
+console.log(media.toString());
+console.log(typeof media);
+console.log(typeof Number);
