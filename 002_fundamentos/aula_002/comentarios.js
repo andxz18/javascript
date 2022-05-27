ea9358c095976bcea684f14293f364052909dc4c
André Macuco <amacuco633@gmail.com>
2022-05-27 17:29:22 -0300

    Atualizado 27/05/22

---
+++ 002_fundamentos/aula_002/comentarios.js
@@ -0,0 +1,12 @@
+// Comentário de uma linha
+
+console.log("Linha 1");
+
+/*
+    Comentário
+    de multiplas linhas
+ */
+
+// ! Evitar comentários desnecessários
+
+// * Comentar tudo que sentir necessidade
