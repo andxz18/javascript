16f51742233e04ba465ed41b9beec56f30ec1687
André Macuco <amacuco633@gmail.com>
2022-05-29 16:38:29 -0300

    Atualizado 29/05/22

--- 002_fundamentos/aula_013/funcao_ex02.js
+++ 002_fundamentos/aula_013/funcao_ex02.js
@@ -16,6 +16,9 @@ console.log(soma(2, 3));
 const subtracao = (a, b) => a - b;
 console.log(subtracao(2, 3));
 
+const imprimir2 = (a) => console.log(a);
+imprimir2("Legal!");
+
 console.log("---");
 
 const parImp = function (num) {
