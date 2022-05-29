b7881c19cc7da9d52301921d148717aeb5c9b63b
André Macuco <amacuco633@gmail.com>
2022-05-29 02:00:27 -0300

    Atualizado 29/05/22

---
+++ 002_fundamentos/aula_007/templatestring.js
@@ -0,0 +1,13 @@
+const nome = "Rebeca";
+const concat = "Olá" + nome + "!";
+const template = `
+    Olá
+    ${nome}!`;
+console.log(concat, template);
+
+// expressões...
+
+console.log(`1 + 1 = ${1 + 1}`);
+
+const up = (texto) => texto.toUpperCase();
+console.log(`Ei... ${up("Cuidado")}!`);
