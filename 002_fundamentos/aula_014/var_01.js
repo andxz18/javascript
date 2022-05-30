fa6bfba767ea9aec7f19300f03b200a8550651df
André Macuco <amacuco633@gmail.com>
2022-05-30 03:14:20 -0300

    Atualizado 30/05/22

---
+++ 002_fundamentos/aula_014/var_01.js
@@ -0,0 +1,19 @@
+{
+  {
+    {
+      {
+        var sera = "Será???"; // variavel global
+        console.log(sera);
+      }
+    }
+  }
+}
+
+console.log(sera);
+
+function teste() {
+  var local = 123;
+  console.log(local);
+}
+
+teste();
