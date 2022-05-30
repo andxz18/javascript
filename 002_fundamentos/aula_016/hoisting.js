d3f6399fcedcaaa58aa5abd14892cdf61b2ad93d
André Macuco <amacuco633@gmail.com>
2022-05-30 03:54:38 -0300

    Atualizado 30/05/22

---
+++ 002_fundamentos/aula_016/hoisting.js
@@ -0,0 +1,8 @@
+// ? Hoisting move a declaração de variáveis para o topo do código
+console.log("a = " + a);
+var a = 2;
+console.log("a = " + a);
+
+console.log("b = " + b);
+let b = 2;
+console.log("b = " + b);
